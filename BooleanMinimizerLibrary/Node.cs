65065452aea65e3db1663ae5a5cb19f497f67c4c
GrishanyaaShustov <dinsuil275283@gmail.com>
2025-04-10 11:22:39 +0500

    Create Node.cs
    Create function to vector convert

---
+++ BooleanMinimizerLibrary/Node.cs
@@ -0,0 +1,31 @@
+namespace BooleanMinimizerLibrary
+{
+    public enum NodeType
+    {
+        Variable,
+        Constant,
+        Vector,
+        Not,
+        And,
+        Or,
+        Xor,
+        Implies,
+        Equivalent
+    }
+
+    public class Node
+    {
+        public NodeType Type { get; set; }
+        public string Value { get; set; }
+        public Node Left { get; set; }
+        public Node Right { get; set; }
+
+        public Node(NodeType type, string value = null, Node left = null, Node right = null)
+        {
+            Type = type;
+            Value = value;
+            Left = left;
+            Right = right;
+        }
+    }
+}

e29d9bbadb269caca768751cbb10b902bb6dce57
GrishanyaaShustov <dinsuil275283@gmail.com>
2025-04-10 11:20:16 +0500

    Update BooleanMinimizatorModel.cs
    added function to vector conversion

--- BooleanMinimizator/Models/BooleanMinimizatorModel.cs
+++ BooleanMinimizator/Models/BooleanMinimizatorModel.cs
@@ -1,8 +1,13 @@
-namespace BooleanMinimizator.Models;
-
-public class BooleanMinimizatorModel
+namespace BooleanMinimizator.Models
 {
-    public string InputFunction { get; set; }  // сюда пользователь вводит функцию
+    public class BooleanMinimizatorModel
+    {
+        public string InputFunction { get; set; }  // сюда пользователь вводит функцию
 
-    public string ResultMessage { get; set; }  // сюда выводится результат: успех или текст ошибки
+        public string ResultMessage { get; set; }  // сюда выводится результат: успех или текст ошибки
+
+        public string? PolizOutput { get; set; }   // сюда выводится ПОЛИЗ
+
+        public string? VectorOutput { get; set; }  // сюда выводится вектор функции
+    }
 }

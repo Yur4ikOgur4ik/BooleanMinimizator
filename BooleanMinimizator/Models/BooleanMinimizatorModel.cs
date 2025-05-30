00571e20ae123387bfaba4e2d7a0e63e05b79dee
ObemaM <barackobama0405@gmail.com>
2025-05-31 00:13:46 +0500

    haha comit...

--- BooleanMinimizator/Models/BooleanMinimizatorModel.cs
+++ BooleanMinimizator/Models/BooleanMinimizatorModel.cs
@@ -22,7 +22,7 @@ namespace BooleanMinimizator.Models
         public string SKNFOutput { get; set; }      // Совершенная конъюнктивная нормальная форма
 
         public string SDNFOutput { get; set; }      // Совершенная дизъюнктивная нормальная форма
-        
+
         public string MKNFOutput { get; set; }      // Минимальная конъюнктивная нормальная форма
 
         public string MDNFOutput { get; set; }      // Минимальная дизъюнктивная нормальная форма
@@ -36,100 +36,161 @@ namespace BooleanMinimizator.Models
         public List<Area> ZeroAreas { get; set; }
 
         public List<string> Variables { get; set; }
+        // Для области единиц (МДНФ)
         public string GetExpressionForArea(Area area, List<string> variables)
         {
-            if (area == null || variables == null || variables.Count == 0 || KarnaughMap == null)
+            if (area == null || variables == null || variables.Count == 0 || KarnaughMap == null || KarnaughMap.Count == 0)
                 return "";
 
-            int rowCount = KarnaughMap.Count - 1; // без заголовков
-            int colCount = KarnaughMap[0].Count - 1;
+            // Разбор заголовка карты
+            string header = KarnaughMap[0][0];
+            string[] parts = header.Split('\\');
+            if (parts.Length < 2)
+                parts = header.Split(new[] { "\\\\" }, StringSplitOptions.None);
+            if (parts.Length < 2)
+                return "";
 
-            var literals = new List<string>();
+            List<string> rowVars = parts[0].Select(c => c.ToString()).ToList();
+            List<string> colVars = parts[1].Select(c => c.ToString()).ToList();
 
-            for (int r = area.StartRow; r < area.StartRow + area.Height; r++)
+            // Словарь для информации о переменных
+            Dictionary<string, (string type, int index)> varInfo = new Dictionary<string, (string, int)>();
+            for (int i = 0; i < rowVars.Count; i++)
+                varInfo[rowVars[i]] = ("row", i);
+            for (int i = 0; i < colVars.Count; i++)
+                varInfo[colVars[i]] = ("col", i);
+
+            // Собираем значения переменных в области
+            Dictionary<string, HashSet<int>> variableValues = new Dictionary<string, HashSet<int>>();
+            foreach (var v in variables)
             {
-                for (int c = area.StartCol; c < area.StartCol + area.Width; c++)
+                if (varInfo.ContainsKey(v))
+                    variableValues[v] = new HashSet<int>();
+            }
+
+            int numRows = KarnaughMap.Count - 1;
+            int numCols = KarnaughMap[0].Count - 1;
+
+            // Анализируем все ячейки области
+            for (int dr = 0; dr < area.Height; dr++)
+            {
+                for (int dc = 0; dc < area.Width; dc++)
                 {
-                    int wrappedRow = ((r % (rowCount - 1)) + 1); // пропуск заголовка строк
-                    int wrappedCol = ((c % (colCount - 1)) + 1); // пропуск заголовка столбцов
+                    int r = (area.StartRow + dr) % numRows;
+                    int c = (area.StartCol + dc) % numCols;
 
-                    if (KarnaughMap[wrappedRow][wrappedCol] == "1")
+                    // Получаем битовые комбинации
+                    List<int> rowBits = GetGrayCode(r, rowVars.Count);
+                    List<int> colBits = GetGrayCode(c, colVars.Count);
+
+                    // Собираем значения переменных
+                    foreach (var v in variableValues.Keys)
                     {
-                        for (int i = 0; i < variables.Count; i++)
-                        {
-                            bool isFixed = false;
-                            if (i == 0) // переменная по строкам
-                            {
-                                isFixed = (r / Math.Pow(2, variables.Count - 1 - i)) % 2 == 1;
-                            }
-                            else // переменные по столбцам
-                            {
-                                isFixed = (c / Math.Pow(2, variables.Count - 1 - i)) % 2 == 1;
-                            }
-
-                            if (isFixed)
-                            {
-                                literals.Add(variables[i]);
-                            }
-                            else
-                            {
-                                literals.Add($"¬{variables[i]}");
-                            }
-                        }
-                        return $"({string.Join(" ∧ ", literals)})";
+                        var (type, index) = varInfo[v];
+                        int value = type == "row" ? rowBits[index] : colBits[index];
+                        variableValues[v].Add(value);
                     }
                 }
             }
 
-            return "";
+            // Формируем литералы для МДНФ
+            List<string> literals = new List<string>();
+            foreach (var v in variableValues.Keys)
+            {
+                if (variableValues[v].Count == 1)
+                {
+                    int value = variableValues[v].First();
+                    literals.Add(value == 1 ? v : $"¬{v}");
+                }
+            }
+
+            return $"({string.Join(" ∧ ", literals)})";
         }
+
+        // Для области нулей (МКНФ)
         public string GetExpressionForZeroArea(Area area, List<string> variables)
         {
-            if (area == null || variables == null || variables.Count == 0 || KarnaughMap == null)
+            if (area == null || variables == null || variables.Count == 0 || KarnaughMap == null || KarnaughMap.Count == 0)
                 return "";
 
-            int rowCount = KarnaughMap.Count - 1;
-            int colCount = KarnaughMap[0].Count - 1;
+            // Разбор заголовка карты
+            string header = KarnaughMap[0][0];
+            string[] parts = header.Split('\\');
+            if (parts.Length < 2)
+                parts = header.Split(new[] { "\\\\" }, StringSplitOptions.None);
+            if (parts.Length < 2)
+                return "";
 
-            var literals = new List<string>();
-            bool found = false;
+            List<string> rowVars = parts[0].Select(c => c.ToString()).ToList();
+            List<string> colVars = parts[1].Select(c => c.ToString()).ToList();
 
-            for (int r = area.StartRow; r < area.StartRow + area.Height && !found; r++)
+            // Словарь для информации о переменных
+            Dictionary<string, (string type, int index)> varInfo = new Dictionary<string, (string, int)>();
+            for (int i = 0; i < rowVars.Count; i++)
+                varInfo[rowVars[i]] = ("row", i);
+            for (int i = 0; i < colVars.Count; i++)
+                varInfo[colVars[i]] = ("col", i);
+
+            // Собираем значения переменных в области
+            Dictionary<string, HashSet<int>> variableValues = new Dictionary<string, HashSet<int>>();
+            foreach (var v in variables)
             {
-                for (int c = area.StartCol; c < area.StartCol + area.Width && !found; c++)
+                if (varInfo.ContainsKey(v))
+                    variableValues[v] = new HashSet<int>();
+            }
+
+            int numRows = KarnaughMap.Count - 1;
+            int numCols = KarnaughMap[0].Count - 1;
+
+            // Анализируем все ячейки области
+            for (int dr = 0; dr < area.Height; dr++)
+            {
+                for (int dc = 0; dc < area.Width; dc++)
                 {
-                    int wrappedRow = ((r % (rowCount - 1)) + 1); // Пропуск заголовка строк
-                    int wrappedCol = ((c % (colCount - 1)) + 1); // Пропуск заголовка столбцов
+                    int r = (area.StartRow + dr) % numRows;
+                    int c = (area.StartCol + dc) % numCols;
 
-                    if (KarnaughMap[wrappedRow][wrappedCol] == "0")
+                    // Получаем битовые комбинации
+                    List<int> rowBits = GetGrayCode(r, rowVars.Count);
+                    List<int> colBits = GetGrayCode(c, colVars.Count);
+
+                    // Собираем значения переменных
+                    foreach (var v in variableValues.Keys)
                     {
-                        for (int i = 0; i < variables.Count; i++)
-                        {
-                            bool isFixed = false;
-                            if (i == 0) // первая переменная по строкам
-                            {
-                                isFixed = (r / Math.Pow(2, variables.Count - 1 - i)) % 2 == 1;
-                            }
-                            else // остальные по столбцам
-                            {
-                                isFixed = (c / Math.Pow(2, variables.Count - 1 - i)) % 2 == 1;
-                            }
-
-                            if (isFixed)
-                            {
-                                literals.Add(variables[i]);
-                            }
-                            else
-                            {
-                                literals.Add($"¬{variables[i]}");
-                            }
-                        }
-                        found = true;
+                        var (type, index) = varInfo[v];
+                        int value = type == "row" ? rowBits[index] : colBits[index];
+                        variableValues[v].Add(value);
                     }
+                }
+            }
+
+            // Формируем литералы для МКНФ
+            List<string> literals = new List<string>();
+            foreach (var v in variableValues.Keys)
+            {
+                if (variableValues[v].Count == 1)
+                {
+                    int value = variableValues[v].First();
+                    literals.Add(value == 0 ? v : $"¬{v}");
                 }
             }
 
             return $"({string.Join(" ∨ ", literals)})";
+        }
+
+        // Вспомогательный метод для получения кода Грея
+        private List<int> GetGrayCode(int index, int numBits)
+        {
+            if (numBits == 0)
+                return new List<int>();
+
+            int gray = index ^ (index >> 1);
+            List<int> bits = new List<int>();
+            for (int i = numBits - 1; i >= 0; i--)
+            {
+                bits.Add((gray >> i) & 1);
+            }
+            return bits;
         }
     }
 }
